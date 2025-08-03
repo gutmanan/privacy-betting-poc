a19595ef1bbd777df11748d1673546ed86a67c92
gutmanan <gutmanan@gmail.com>
2025-08-03 13:42:34 +0300

    Add Hardhat configuration

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
